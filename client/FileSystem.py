14133c842fe362816e9576fd19fbee7a30aa4e0a
GGnando <nelio1996@gmail.com>
2018-12-03 19:54:35 -0500

    parser

--- client/FileSystem.py
+++ client/FileSystem.py
@@ -1,5 +1,8 @@
 import MemoryInterface, AbsolutePathNameLayer
 import time
+
+allowed_commands = ["mkdir", "create", "mv", "read", "write", "status", "rm", "exit"]
+
 def Initialize_My_FileSystem():
     MemoryInterface.Initialize_My_FileSystem()
     AbsolutePathNameLayer.AbsolutePathNameLayer().new_entry('/', 1)
@@ -171,8 +174,6 @@ def test_move():
 
 
 
-
-
     
 def test_link():
     my_object, files_to_write_to, data_to_write = test_write_2D()
@@ -192,11 +193,110 @@ def test_link():
     my_object.mv("/A/A_file1", "/A/B/")
     my_object.status()
 
+
+def client():
+    Initialize_My_FileSystem()
+    interface_client = FileSystemOperations()
+
+    while(1):
+        user_input = raw_input("$ ")
+        tokenized_input = user_input.split()
+        if len(tokenized_input) == 0:
+            print("Error: input mismatch")
+        elif tokenized_input[0] not in allowed_commands:
+            print("Error: " + tokenized_input[0] + " is not an allowed_command")
+        else:
+            if tokenized_input[0] == allowed_commands[0]: #mkdir
+                del tokenized_input[0]
+                if(len(tokenized_input) != 1):
+                    print("Error: argument mismatch")
+                else:
+                    interface_client.mkdir(tokenized_input[0])
+                
+
+
+            elif tokenized_input[0] == allowed_commands[1]: #create
+                del tokenized_input[0]
+                if(len(tokenized_input) != 1):
+                    print("Error: argument mismatch")
+                else:
+                    interface_client.create(tokenized_input[0])
+
+
+
+            elif tokenized_input[0] == allowed_commands[2]: #mv
+                del tokenized_input[0]
+                if(len(tokenized_input) != 2):
+                    print("Error: argument mismatch")
+                else:
+                    interface_client.mv(tokenized_input[0], tokenized_input[1])
+
+
+
+            elif tokenized_input[0] == allowed_commands[3]: #read
+                del tokenized_input[0]
+                if ((len(tokenized_input) not in range(1,4))):
+                    print("Error: argument mismatch")
+                elif (len(tokenized_input) == 3) and (not tokenized_input[1].isdigit() or not  tokenized_input[2].isdigit()):
+                    print("Error: argument mismatch")
+
+                elif(len(tokenized_input) == 2) and not tokenized_input[1].isdigit():
+                    print("Error: argument mismatch")
+                else:
+                    offset = 0
+                    size = -1
+                    if(len(tokenized_input) == 2):
+                        offset = int(tokenized_input[1])
+                    elif(len(tokenized_input) == 3):
+                        offset = int(tokenized_input[1])
+                        size = int(tokenized_input[2])
+                    interface_client.read(tokenized_input[0],offset, size)
+
+
+
+            elif tokenized_input[0] == allowed_commands[4]: #write
+                del tokenized_input[0]
+                if(len(tokenized_input) not in range(2, 4)):
+                    print("Error: argument mismatch")
+                elif(len(tokenized_input) == 3 and not tokenized_input[2].isdigit()):
+                    print("Error: argument mismatch")
+                else:
+                    offset = 0
+                    if(len(tokenized_input) == 3):
+                        offset = int(tokenized_input[2])
+                    interface_client.write(tokenized_input[0], tokenized_input[1], offset)
+
+
+            elif tokenized_input[0] == allowed_commands[5]: #status
+                del tokenized_input[0]
+                if(len(tokenized_input) != 0):
+                    print("Error: argument mismatch")
+                else:
+                    #do 
+                    interface_client.status()
+
+
+
+            elif tokenized_input[0] == allowed_commands[6]: #rm
+                del tokenized_input[0]
+                if(len(tokenized_input) != 1):
+                    print("Error: argument mismatch")
+                else:
+                    interface_client.rm(tokenized_input[0])
+
+
+            elif tokenized_input[0] == allowed_commands[7]: #exit
+                del tokenized_input[0]
+                if(len(tokenized_input[0] != 0)):
+                    print("Error: argument mismatch")
+                else:
+                    exit()
  
 if __name__ == '__main__':
     #test_move()
     # test_link()
-    test_read_and_write('print')
+    #test_write_2D("print")
+    client()
     '''
     start_time = time.time()
     Initialize_My_FileSystem()
